09b820269291ba7eccf0a9d2bd10bdf164afcc3d
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-08-05 22:58:15 +0600

    created db connection

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // ! app initialization using middleware
 var app = express();
 app.use(express.json());
 
-// * Managing the routes
-app.get('/', (req, res) => {
-	res.send('Welcome to Wild Life Camp');
+// ! Connect Database
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@wildlife-camp.chmos0m.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
+
+async function run() {
+	try {
+		// Connect the client to the server
+		await client.connect();
+
+		// * Managing the routes
+		app.get('/', (req, res) => {
+			res.send('Welcome to Wild Life Camp');
+		});
+
+		// Send a ping to confirm a successful connection
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} catch (err) {
+		console.log(err);
+	}
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
 	console.log(`Server is running at port ${port}`);
